68ee6aaf50a66e352e2448cf984e1a527cfcb161
ripmeep <36815692+ripmeep@users.noreply.github.com>
2021-11-08 02:14:54 +0000

    Add files via upload

---
+++ test.rb
@@ -0,0 +1,24 @@
+require './waptools.so'
+
+$interface = ARGV[0]
+
+usage = Proc.new { puts "Usage: ruby #{__FILE__} [wlan interface]"; exit }
+usage.call unless !$interface.nil?
+
+puts "#{WAPTools::authors}\n\n"
+
+scanner = WAPTools::Scanner.new($interface)
+aps = scanner.scan
+
+aps.each { |ap|
+#   Note: ap.to_s will output all details of the nearby AP
+#	puts ap.to_s
+
+	puts "SSID     : #{ap.ssid}"
+	puts "BSSID    : #{ap.bssid}"	
+	puts "Frequency: #{ap.frequency}"
+	puts "Bitrate  : #{ap.bitrate}"
+	puts "Stats    : #{ap.stats}"
+	puts "Channel  : #{ap.channel}"
+	puts
+}
